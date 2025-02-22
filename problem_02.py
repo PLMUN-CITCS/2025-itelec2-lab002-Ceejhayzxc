741e785dbd4dab371d83b4d86e833fa87a2acac4
Ceejhayzxc <christianjorgetolda_bsit@plmun.edu.ph>
2025-02-22 13:12:03 +0800

    Submitting Python Session 03 - Exercise 01

--- problem_02.py
+++ problem_02.py
@@ -1,12 +1,12 @@
-# Tolda, Christian Jorge M.
+# YOUR NAME
 # ITELEC2
 # Problem Set 01 - Problem 02
 # Square the Number Program
 
 def main():
-	print("Square the Number Program")
+ print("Square the Number Program")
+ number = int(input("Enter a number: "))
+ print(f"The square of {number} is {(number * number):.2f}")
 
-	if __name__ == "__main__":
-	main()
-	number = int(input("Enter a number: "))
-	print(f"The square of {number} is {(number * number):.2f}")
+if __name__ == "__main__":
+    main()
